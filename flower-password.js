d2f141c34ee1d7582433e0d6f399302758785795
xLsDg <xlsdg@qq.com>
2016-09-08 11:02:46 +0800

    1.修改双引号为单引号；

--- flower-password.js
+++ flower-password.js
@@ -13,29 +13,29 @@
 // @license     MIT License
 // ==/UserScript==
 
-function countCode(password, key){
-    if(password && key){
-        var md5one = md5(password,key);
-        var md5two = md5(md5one,'snow');
-        var md5three = md5(md5one,'kise');
+function countCode(password, key) {
+    if (password && key) {
+        var md5one = md5(password, key);
+        var md5two = md5(md5one, 'snow');
+        var md5three = md5(md5one, 'kise');
         //计算大小写
-        var rule = md5three.split("");
-        var source = md5two.split("");
-        for(var i=0;i<=31;i++){ 
-            if(isNaN(source[i])){
-                var str ="sunlovesnow1990090127xykab";
-                if(str.search(rule[i]) > -1){
+        var rule = md5three.split('');
+        var source = md5two.split('');
+        for (var i = 0; i <= 31; i++) {
+            if (isNaN(source[i])) {
+                var str = 'sunlovesnow1990090127xykab';
+                if (str.search(rule[i]) > -1) {
                     source[i] = source[i].toUpperCase();
                 }
             }
         }
-        var code32 = source.join("");
-        var code1 = code32.slice(0,1);
-        var code16 = "";
-        if(isNaN(code1)){
-            code16 = code32.slice(0,16);
-        }else{
-            code16 = "K" + code32.slice(1,16);
+        var code32 = source.join('');
+        var code1 = code32.slice(0, 1);
+        var code16 = '';
+        if (isNaN(code1)) {
+            code16 = code32.slice(0, 16);
+        } else {
+            code16 = 'K' + code32.slice(1, 16);
         }
         return [code16, code32];
     }
@@ -43,6 +43,7 @@ function countCode(password, key){
 
 $(function() {
     var currentField = null;
+
     function setupInputListeners() {
         function insideBox(e) {
             return e.parents('#flower-password-input').size() > 0;
@@ -58,7 +59,7 @@ $(function() {
             currentField = $(this);
             var offset = currentField.offset();
             var height = currentField.outerHeight();
-            $('#flower-password-input').css({left: offset.left + "px", top: offset.top + height + "px"}).show();
+            $('#flower-password-input').css({ left: offset.left + 'px', top: offset.top + height + 'px' }).show();
         });
         $(document).on('focus', 'input:not(:password)', function() {
             if (insideBox($(this))) {
@@ -81,17 +82,17 @@ $(function() {
         );
         $('body').append(
             '<div id="flower-password-input" style="display: none;">' +
-                '<span id="flower-password-close" title="关闭">关闭</span>' +
-                '<h1>花密 Flower Password</h1>' +
-                '<label for="flower-password-password">记忆密码</label><input id="flower-password-password" name="flower-password-password" type="password" value="" maxlength="20" />' +
-                '<br>' +
-                '<label for="flower-password-key">区分代号</label><input id="flower-password-key" name="flower-password-key" type="text" value="" maxlength="20" />' +
-                '<p id="flower-password-hint">· 记忆密码：可选择一个简单易记的密码，用于生成其他高强度密码。<br>· 区分代号：用于区别不同用途密码的简短代号，如淘宝账号可用“taobao”或“tb”等。<br>· 按Enter键或Esc键关闭本窗口。<br>· 花密官网地址：<a href="http://flowerpassword.com/" target="_blank">http://flowerpassword.com/</a></p>' +
+            '<span id="flower-password-close" title="关闭">关闭</span>' +
+            '<h1>花密 Flower Password</h1>' +
+            '<label for="flower-password-password">记忆密码</label><input id="flower-password-password" name="flower-password-password" type="password" value="" maxlength="20" />' +
+            '<br>' +
+            '<label for="flower-password-key">区分代号</label><input id="flower-password-key" name="flower-password-key" type="text" value="" maxlength="20" />' +
+            '<p id="flower-password-hint">· 记忆密码：可选择一个简单易记的密码，用于生成其他高强度密码。<br>· 区分代号：用于区别不同用途密码的简短代号，如淘宝账号可用“taobao”或“tb”等。<br>· 按Enter键或Esc键关闭本窗口。<br>· 花密官网地址：<a href="http://flowerpassword.com/" target="_blank">http://flowerpassword.com/</a></p>' +
             '</div>'
         );
         var onChange = function() {
-            var password = $("#flower-password-password").val();
-            var key = $("#flower-password-key").val();
+            var password = $('#flower-password-password').val();
+            var key = $('#flower-password-key').val();
             var result = countCode(password, key);
             if (result) {
                 currentField.val(result[0]);
